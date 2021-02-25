12a3d3e1d6ea0a1be0c94862cbddb31a79e45898
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2021-02-25 12:20:31 -0500

    Require Schema, connect Mongoose, create test route and add test to DB

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/Campground");
+
+mongoose
+  .connect("mongodb://localhost/yelp-camp", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("CONNECTION SUCCESS!");
+  })
+  .catch((err) => {
+    console.LOG("Error connecting to mongo", err);
+  });
+
+const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({ title: "My Backyard" });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
