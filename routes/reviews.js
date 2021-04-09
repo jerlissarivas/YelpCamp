bbf5a84fbc2cf2d9302fdf8948cf1bf22ccb6d12
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2021-04-08 22:13:01 -0400

    Add middleware to routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { validateReview } = require("../middleware");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 const Campground = require("../models/Campground");
 const Review = require("../models/Review");
-
 
 const ExpressError = require("../utilities/ExpressError");
 const catchAsync = require("../utilities/catchAsync");
 
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -24,6 +25,8 @@ router.post(
 
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
